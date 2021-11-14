6c02933419bdb49313075197ad5382b12914a02c
Mohan varma <80708364+Mohan-varma9999@users.noreply.github.com>
2021-11-14 14:14:12 +0530

    Create <6_Best_time_to_buy_and_sell_stock>.cpp

---
+++ week02/<6_Best_time_to_buy_and_sell_stock>.cpp
@@ -0,0 +1,16 @@
+//leetcode
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0, minPrice = INT_MAX;
+        for (int i=0;i<prices.size();i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i]; 
+            }
+            else {
+                profit = max(profit, prices[i]-minPrice);
+            }
+        }
+        return profit;
+    }
+};
